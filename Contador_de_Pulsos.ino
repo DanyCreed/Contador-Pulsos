9d81ee64a11d5889fd0fe725560c01cca9beae34
DanyCreed <dtoledop93@gmail.com>
2016-04-27 20:12:28 -0500

    Added files via upload

---
+++ Contador_de_Pulsos.ino
@@ -0,0 +1,33 @@
+
+
+
+int conta = 0;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(2,INPUT);
+ pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(2)==HIGH)
+  {
+    if(digitalRead(2)==LOW)
+    {
+      conta++;
+      Serial.println(conta);
+      delay (100);
+    }
+  }
+
+  if(conta==5)
+  {
+    digitalWrite(13,HIGH);
+    }
+    if(conta==8)
+    {
+      digitalWrite(13,LOW);
+      }
+}
